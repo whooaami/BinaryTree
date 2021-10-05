6770d033198847cde6552c27e5e3675812e4712b
whooaami <yarkos03@gmail.com>
2021-10-05 22:08:39 +0300

    Adding delete method to binary tree

--- binary_tree.py
+++ binary_tree.py
@@ -1,3 +1,15 @@
+import math
+
+
+class Node:
+
+    def __init__(self, value, parent=None):
+        self.right = None
+        self.left = None
+        self.parent = None
+        self.value = value
+
+
 class Tree:
 
     def __init__(self):
@@ -10,6 +22,34 @@ class Tree:
         return True
 
     def delete(self, value):
+        node_to_delete = self._search(self.root, value)
+        if node_to_delete is None:
+            return
+        elif not node_to_delete.left and not node_to_delete.right:
+            self._delete_leaf(node_to_delete)
+        elif node_to_delete.left and not node_to_delete.right:
+            self._delete_node_with_left_child(node_to_delete)
+        elif node_to_delete.right and not node_to_delete.left:
+            self._delete_node_with_right_child(node_to_delete)
+        else:
+            self._delete_root(node_to_delete)
+
+    def _delete_leaf(self, node_to_delete):
+        if node_to_delete.parent.left == node_to_delete:
+            node_to_delete.parent.left = None
+        else:
+            node_to_delete.parent.right = None
+        return
+
+    def _delete_node_with_right_child(self, node_to_delete):
+        node_to_delete.value = node_to_delete.right.value
+        node_to_delete.right = None
+
+    def _delete_node_with_left_child(self, node_to_delete):
+        node_to_delete.value = node_to_delete.left.value
+        node_to_delete.left = None
+
+    def _delete_root(self, node_to_delete):
         pass
 
     def max_value(self):
@@ -56,7 +96,7 @@ class Tree:
             # go left
             return self._search(node_to_check.left, value)
 
-    def bypass (self, root):
+    def bypass(self, root):
         result = []
         if root:
             result.append(root.value)
@@ -64,28 +104,106 @@ class Tree:
             result += self.bypass(root.right)
         return result
 
+    # printing graph stuff
+    def print_ascii_graph(self):
+        pairs = self._get_pairs_in_order()
+        self.print_by_lines(pairs)
 
-class Node:
+    def print_spaces(self, num):
+        str = num * " "
+        print(str, end="")
 
-    def __init__(self, value, parent=None):
-        self.right = None
-        self.left = None
-        self.parent = None
-        self.value = value
+    def print_arrows(self, nodes_count, space_from_start):
+        self.print_spaces(space_from_start)
+        for i in range(nodes_count // 2):
+            print("/", end="")
+            self.print_spaces(space_from_start * 2)
+            print("\\", end="")
+            self.print_spaces(space_from_start * 2)
+
+    def print_nodes(self, nodes, space_from_start):
+        self.print_spaces(space_from_start)
+        while nodes:
+            print(nodes.pop(0), end="")
+            self.print_spaces(space_from_start * 2)
+
+    def print_by_lines(self, pairs):
+
+        if self.root is None:
+            return
+
+        count_pairs = len(pairs)
+        count_lines = int(math.log2(count_pairs)) + 1
+        nums_on_last_line = pow(count_lines, count_lines - 1)
+
+        pairs_per_line = 1
+        spaces_from_start = nums_on_last_line // 2
+
+        # print root
+        self.print_spaces(spaces_from_start * 2)
+        print(self.root.value)
+
+        for i in range(count_lines):
+
+            nums_in_line = []
+
+            for j in range(pairs_per_line):
+                if not pairs:
+                    break
+
+                # print(pairs.pop(0), end=" ")
+                pair = pairs.pop(0)
+                nums_in_line.append(pair[0])
+                nums_in_line.append(pair[1])
+
+            self.print_arrows(len(nums_in_line), spaces_from_start)
+            print()
+            self.print_nodes(nums_in_line, spaces_from_start)
+            print()
+
+            spaces_from_start //= 2
+
+            pairs_per_line *= 2
+
+    def _get_pairs_in_order(self):
+        if self.root is None:
+            return
+
+        queue = [self.root]
+        pairs = []
+        for i in queue:
+            pair = []
+            if i.left is not None:
+                queue.append(i.left)
+                pair.append(i.left.value)
+            else:
+                pair.append("")
+
+            if i.right is not None:
+                queue.append(i.right)
+                pair.append(i.right.value)
+            else:
+                pair.append("")
+
+            pairs.append(pair)
+
+        return pairs
 
 
+if __name__ == '__main__':
+    tree = Tree()
+    tree.insert(15)
+    tree.insert(6)
+    tree.insert(7)
+    tree.insert(4)
+    tree.insert(5)
+    tree.insert(23)
+    tree.insert(69)
+    tree.insert(30)
 
-tree = Tree()
-tree.insert(15)
-tree.insert(6)
-tree.insert(7)
-tree.insert(4)
-tree.insert(5)
-tree.insert(23)
-tree.insert(69)
-tree.insert(30)
+    tree.delete(6)
+    tree.delete(69)
+    tree.delete(69)
 
-print(tree.search(10))
-print(tree.search(4))
-print(tree.search(6))
-print(tree.bypass(tree.root))
+    print(tree.bypass(tree.root))
+    print(tree.print_ascii_graph())
